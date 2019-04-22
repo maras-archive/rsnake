0eddd39d567785fa6b413bb3e956601c8ba69a6e
schulke-214 <schulkemaximilian@gmail.com>
2019-04-22 15:43:47 +0200

    feat: added imports for all modules and extern crates and create a basic window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+extern crate piston_window;
+extern crate rand;
+
+mod draw;
+mod fruit;
+mod game;
+mod snake;
+
+use piston_window::*;
+
+const BACKGROUND: types::Color = [0.0, 0.0, 0.0, 1.0];
+
 fn main() {
-    println!("Hello, world!");
+    let state = game::State::new();
+    let mut window: PistonWindow = WindowSettings::new("rsnake", [640, 640])
+        .exit_on_esc(true)
+        .resizable(false)
+        .build()
+        .unwrap();
+
+    while let Some(e) = window.next() {
+        window.draw_2d(&e, |_c, g| {
+            clear(BACKGROUND, g);
+        });
+    }
 }
