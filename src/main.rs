eddee9694fadacbbd8f5aa22cb051869432eed58
Mara Schulke <mara@schulke.xyz>
2019-04-23 12:58:05 +0200

    fix: removed 'extern crate' since its not usefull anymore

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,35 @@
-extern crate piston_window;
-extern crate rand;
-
 mod draw;
 mod fruit;
 mod game;
+mod physics;
 mod snake;
 
+use game::Game;
 use piston_window::*;
 
-const BACKGROUND: types::Color = [0.0, 0.0, 0.0, 1.0];
-
 fn main() {
-    let state = game::State::new();
-    let mut window: PistonWindow = WindowSettings::new("rsnake", [640, 640])
+    let (width, height) = (30, 30);
+
+    let mut window: PistonWindow = WindowSettings::new("rsnake", [width * 25, height * 25])
         .exit_on_esc(true)
         .resizable(false)
         .build()
         .unwrap();
 
-    while let Some(e) = window.next() {
-        window.draw_2d(&e, |_c, g| {
-            clear(BACKGROUND, g);
+    let mut game = Game::new(width, height);
+
+    while let Some(event) = window.next() {
+        // if let Some(Button::Keyboard(key)) = event.press_args() {
+        //     game.key_pressed(key);
+        // }
+
+        window.draw_2d(&event, |c, g| {
+            clear([0.0, 0.0, 0.0, 1.0], g);
+            // game.draw(&c, g);
         });
+
+        //     event.update(|arg| {
+        //         game.update(arg.dt);
+        //     });
     }
 }
