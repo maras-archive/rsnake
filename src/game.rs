0f036b3e5f1865bf4968fbe22bed1b15c931da95
Mara Schulke <mara@schulke.xyz>
2019-04-23 23:16:45 +0200

    feat(game): added update and draw hooks

--- src/game.rs
+++ src/game.rs
@@ -1,6 +1,6 @@
 use piston_window::*;
 
-use crate::{draw, fruit::Fruit, snake::Snake};
+use crate::{colors, draw::*, fruit::Fruit, physics::Position, snake::Snake};
 
 pub struct Game {
     snake: Snake,
@@ -14,7 +14,7 @@ pub struct Game {
 impl Game {
     pub fn new(width: u32, height: u32) -> Self {
         Self {
-            snake: Snake::new(),
+            snake: Snake::new(5, 5),
             fruit: Fruit::new(),
             size: (width, height),
             score: 0,
@@ -23,13 +23,46 @@ impl Game {
         }
     }
 
-    // pub fn start(&mut self) {
-    //     self.paused = false;
-    // }
-
-    pub fn draw() {
-        println!("im drawing the game")
+    pub fn start(&mut self) {
+        self.paused = false;
     }
 
-    pub fn update() {}
+    pub fn draw(&self, ctx: Context, g: &mut G2d) {
+        self.draw_border(&ctx, g);
+        self.snake.draw(&ctx, g);
+    }
+
+    pub fn update(&mut self) {}
+
+    fn draw_border(&self, ctx: &Context, g: &mut G2d) {
+        let [win_width, win_height] = ctx.get_view_size();
+
+        rectangle(
+            colors::BORDER,
+            [0.0, 0.0, BLOCK_SIZE, win_height],
+            ctx.transform,
+            g,
+        );
+
+        rectangle(
+            colors::BORDER,
+            [0.0, 0.0, win_width, BLOCK_SIZE],
+            ctx.transform,
+            g,
+        );
+
+        rectangle(
+            colors::BORDER,
+            [win_width - BLOCK_SIZE, 0.0, BLOCK_SIZE, win_height],
+            ctx.transform,
+            g,
+        );
+
+        rectangle(
+            colors::BORDER,
+            [0.0, win_height - BLOCK_SIZE, win_width, BLOCK_SIZE],
+            ctx.transform,
+            g,
+        );
+    }
 }
