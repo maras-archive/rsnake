64b059f3ff696960c7756888ad9196a5d6d22b27
Mara Schulke <mara@schulke.xyz>
2019-04-23 23:14:08 +0200

    feat: added all colors as constants

---
+++ src/colors.rs
@@ -0,0 +1,6 @@
+use piston_window::types::Color;
+
+pub const BACKGROUND: Color = [0.0, 0.0, 0.0, 1.0];
+pub const BORDER: Color = [0.2, 0.2, 0.2, 1.0];
+pub const SNAKE: Color = [0.1, 0.9, 0.1, 1.0];
+pub const FRUIT: Color = [1.0, 0.0, 0.0, 1.0];
